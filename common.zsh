1503188efa135d4f0bdc52900fdac5dfd0e91044
Robert David <robert.david.public@gmail.com>
2018-12-12 15:42:35 +0100

    common.zsh: set ls allias correctly

--- common.zsh
+++ common.zsh
@@ -14,7 +14,7 @@ export GIT_AUTHOR_EMAIL="robert.david.public@gmail.com"
 # colored ls and its shortcuts
 export LSCOLORS='exfxcxdxbxegedAbAgacad'
 export LS_COLORS="di=34;40:ln=35;40:so=32;40:pi=33;40:ex=31;40:bd=34;46:cd=34;43:su=1;;41:sg=1;;46:tw=0;42:ow=0;43:"
-ls --color -d . &>/dev/null && alias ls='ls --color=tty' || { ls -G . &>/dev/null && alias ls='ls -G' }
+ls --color -d . &>/dev/null && alias ls='ls --color' || { ls -G . &>/dev/null && alias ls='ls -G' }
 alias ll="ls -lh"
 alias la="ls -A"
 alias l="ls -lA"
